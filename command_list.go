3b5926ce7a58b45cf27b4f549af7380a0c889ed5
QuangTung97 <quangtung29121997@gmail.com>
2021-09-30 18:54:40 +0700

    Stress Test Command List

--- command_list.go
+++ command_list.go
@@ -8,13 +8,26 @@ import (
 	"unsafe"
 )
 
+type atomicUint64 struct {
+	value uint64
+}
+
+func (a *atomicUint64) store(v uint64) {
+	atomic.StoreUint64(&a.value, v)
+}
+
+func (a *atomicUint64) load() uint64 {
+	return atomic.LoadUint64(&a.value)
+}
+
 type commandListStore struct {
-	mut  sync.Mutex
-	cond *sync.Cond
+	mut     sync.Mutex
+	cond    *sync.Cond
+	stopped bool
 
 	buffer     []byte
-	nextOffset uint64
-	processed  uint64
+	nextOffset atomicUint64
+	processed  atomicUint64
 
 	currentCommandData []byte
 }
@@ -26,11 +39,11 @@ type commandListHeader struct {
 }
 
 const commandListHeaderSize = uint64(unsafe.Sizeof(commandListHeader{}))
-const commandListHeaderSizeUint64 = (commandListHeaderSize + 7) / 8
+const commandListHeaderSizeUint64 = (commandListHeaderSize + 7) / 8 // upper bound of dividing by 8
 
 type commandListHeaderData [commandListHeaderSizeUint64]uint64
 
-func getByteArrays(data []uint64) []byte {
+func getCommandHeaderBytes(data []uint64) []byte {
 	var result []byte
 	header := (*reflect.SliceHeader)(unsafe.Pointer(&result))
 	header.Data = uintptr(unsafe.Pointer(&data[0]))
@@ -45,14 +58,26 @@ func initCommandListStore(s *commandListStore, bufSize int) {
 	s.cond = sync.NewCond(&s.mut)
 }
 
-func (s *commandListStore) appendBytes(data []byte) {
-	begin := s.nextOffset
-	copy(s.buffer[begin:], data)
-	s.nextOffset += uint64(len(data))
+func (s *commandListStore) appendBytes(offset uint64, data []byte) uint64 {
+	max := len(s.buffer)
+	index := int(offset) % max
+	copy(s.buffer[index:], data)
+	if index+len(data) > max {
+		firstPart := max - index
+		secondPart := len(data) - firstPart
+		copy(s.buffer[:secondPart], data[firstPart:])
+	}
+	return offset + uint64(len(data))
 }
 
 func (s *commandListStore) readAt(data []byte, pos uint64) {
-	copy(data, s.buffer[pos:])
+	max := len(s.buffer)
+	index := int(pos) % max
+	copy(data, s.buffer[index:])
+	if index+len(data) > max {
+		firstPart := max - index
+		copy(data[firstPart:], s.buffer)
+	}
 }
 
 func (s *commandListStore) appendCommands(ip net.IP, port uint16, data []byte) {
@@ -61,25 +86,26 @@ func (s *commandListStore) appendCommands(ip net.IP, port uint16, data []byte) {
 	length := uint16(len(data))
 
 	var headerDataAligned commandListHeaderData
-	headerData := getByteArrays(headerDataAligned[:])
-
+	headerData := getCommandHeaderBytes(headerDataAligned[:])
 	header := (*commandListHeader)(unsafe.Pointer(&headerData[0]))
 	copy(header.ip[:], ip.To4())
 	header.port = port
 	header.length = length
 
-	s.appendBytes(headerData[:])
-	s.appendBytes(data)
+	offset := s.nextOffset.load()
+	offset = s.appendBytes(offset, headerData[:])
+	offset = s.appendBytes(offset, data)
+	s.nextOffset.store(offset)
 
 	s.mut.Unlock()
 	s.cond.Signal()
 }
 
 func (s *commandListStore) getNextRawCommandList() (rawCommandList, uint64) {
-	begin := s.processed
+	begin := s.processed.load()
 
 	var headerDataAligned commandListHeaderData
-	headerData := getByteArrays(headerDataAligned[:])
+	headerData := getCommandHeaderBytes(headerDataAligned[:])
 	s.readAt(headerData[:], begin)
 	header := (*commandListHeader)(unsafe.Pointer(&headerData[0]))
 
@@ -93,9 +119,33 @@ func (s *commandListStore) getNextRawCommandList() (rawCommandList, uint64) {
 }
 
 func (s *commandListStore) commitProcessedOffset(value uint64) {
-	atomic.StoreUint64(&s.processed, value)
+	s.processed.store(value)
 }
 
 func (s *commandListStore) getCommitProcessed() uint64 {
-	return atomic.LoadUint64(&s.processed)
+	return s.processed.load()
+}
+
+func (s *commandListStore) waitAvailable() bool {
+	s.mut.Lock()
+	for s.nextOffset.load() <= s.processed.load() && !s.stopped {
+		s.cond.Wait()
+	}
+	continued := !s.stopped
+	s.mut.Unlock()
+	return continued
+}
+
+func (s *commandListStore) isCommandAppendable(dataSize int) bool {
+	max := uint64(len(s.buffer))
+	sizeWithHeader := uint64(dataSize) + commandListHeaderSize
+	return max+s.processed.load() >= s.nextOffset.load()+sizeWithHeader
+}
+
+func (s *commandListStore) stopWait() {
+	s.mut.Lock()
+	s.stopped = true
+	s.mut.Unlock()
+
+	s.cond.Signal()
 }
--- command_list_test.go
+++ command_list_test.go
@@ -1,9 +1,13 @@
 package kvstore
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net"
+	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 	"unsafe"
 )
 
@@ -13,11 +17,17 @@ func newCommandListStore() *commandListStore {
 	return s
 }
 
+func newCommandListStoreBuffSize(buffSize int) *commandListStore {
+	s := &commandListStore{}
+	initCommandListStore(s, buffSize)
+	return s
+}
+
 func TestCommandListHeader(t *testing.T) {
 	var headerData [commandListHeaderSize / 8]uint64
 	headerData[0] = 0x1234567801020304
 
-	data := getByteArrays(headerData[:])
+	data := getCommandHeaderBytes(headerData[:])
 	assert.Equal(t, 8, len(data))
 	assert.Equal(t, []byte{0x04, 0x03, 0x02, 0x01, 0x78, 0x56, 0x34, 0x12}, data)
 
@@ -42,8 +52,6 @@ func TestCommandListStore_AppendCommands_Single(t *testing.T) {
 
 func TestCommandListStore_AppendCommands_Multiple(t *testing.T) {
 	p := newCommandListStore()
-
-	assert.Equal(t, uint64(0), p.nextOffset)
 
 	p.appendCommands(net.IPv4(192, 168, 0, 1), 8100, []byte("some-data"))
 	p.appendCommands(net.IPv4(123, 9, 2, 5), 7233, []byte("another-data"))
@@ -76,4 +84,70 @@ func TestCommandListStore_AppendCommands_Multiple(t *testing.T) {
 
 	p.commitProcessedOffset(completedOffset)
 	assert.Equal(t, completedOffset, p.getCommitProcessed())
+}
+
+func TestCommandListStore_WaitAvailable_Single_Command(t *testing.T) {
+	p := newCommandListStore()
+	p.appendCommands(net.IPv4(192, 168, 0, 1), 8100, []byte("some-data"))
+	continued := p.waitAvailable()
+	assert.Equal(t, true, continued)
+}
+
+func TestCommandListStore_WaitAvailable_Stopped(t *testing.T) {
+	p := newCommandListStore()
+	p.stopWait()
+	continued := p.waitAvailable()
+	assert.Equal(t, false, continued)
+}
+
+func TestCommandListStore_WaitAvailable_No_Command(t *testing.T) {
+	p := newCommandListStore()
+
+	called := false
+	go func() {
+		p.waitAvailable()
+		called = true
+	}()
+	time.Sleep(1 * time.Millisecond)
+
+	assert.Equal(t, false, called)
+}
+
+func TestCommandListStore_Stress_Test(t *testing.T) {
+	p := newCommandListStoreBuffSize(197)
+
+	const numCommands = 10000
+	count := uint32(0)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for p.waitAvailable() {
+			cmdList, offset := p.getNextRawCommandList()
+
+			assert.Equal(t, net.IPv4(198, 168, 53, 1).To4(), cmdList.ip)
+			assert.Equal(t, uint16(8765), cmdList.port)
+			p.commitProcessedOffset(offset)
+
+			atomic.AddUint32(&count, 1)
+		}
+	}()
+
+	for i := 0; i < numCommands; i++ {
+		data := []byte(fmt.Sprintf("command-no-%d", i))
+		size := len(data)
+		for !p.isCommandAppendable(size) {
+			//revive:disable-next-line:empty-block
+		}
+		p.appendCommands(net.IPv4(198, 168, 53, 1), 8765, data)
+	}
+
+	for atomic.LoadUint32(&count) < numCommands {
+		//revive:disable-next-line:empty-block
+	}
+	p.stopWait()
+	wg.Wait()
+
+	assert.Equal(t, uint32(numCommands), atomic.LoadUint32(&count))
 }
