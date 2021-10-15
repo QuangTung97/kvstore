a823faae18842951c5c788d4d3d81b8f1e3a1c44
QuangTung97 <quangtung29121997@gmail.com>
2021-10-15 14:04:51 +0700

    Refactor command list test

--- command_list_test.go
+++ command_list_test.go
@@ -30,11 +30,10 @@ func newIPAddr(a, b, c, d byte) IPAddr {
 }
 
 func TestCommandListStore_AppendCommands_Single(t *testing.T) {
-	// TODO convert from p to s
-	p := newCommandListStore()
-	p.appendCommands(newIPAddr(192, 168, 0, 1), 8100, []byte("some-data"))
+	s := newCommandListStore()
+	s.appendCommands(newIPAddr(192, 168, 0, 1), 8100, []byte("some-data"))
 
-	cmdList, _ := p.getNextRawCommandList()
+	cmdList, _ := s.getNextRawCommandList()
 	assert.Equal(t, rawCommandList{
 		ip:   newIPAddr(192, 168, 0, 1),
 		port: 8100,
@@ -43,61 +42,61 @@ func TestCommandListStore_AppendCommands_Single(t *testing.T) {
 }
 
 func TestCommandListStore_AppendCommands_Multiple(t *testing.T) {
-	p := newCommandListStore()
+	s := newCommandListStore()
 
-	p.appendCommands(newIPAddr(192, 168, 0, 1), 8100, []byte("some-data"))
-	p.appendCommands(newIPAddr(123, 9, 2, 5), 7233, []byte("another-data"))
-	p.appendCommands(newIPAddr(89, 0, 3, 6), 7000, []byte("random-data"))
+	s.appendCommands(newIPAddr(192, 168, 0, 1), 8100, []byte("some-data"))
+	s.appendCommands(newIPAddr(123, 9, 2, 5), 7233, []byte("another-data"))
+	s.appendCommands(newIPAddr(89, 0, 3, 6), 7000, []byte("random-data"))
 
-	cmdList, completedOffset := p.getNextRawCommandList()
+	cmdList, completedOffset := s.getNextRawCommandList()
 	assert.Equal(t, rawCommandList{
 		ip:   newIPAddr(192, 168, 0, 1),
 		port: 8100,
 		data: []byte("some-data"),
 	}, cmdList)
 
-	p.commitProcessedOffset(completedOffset)
+	s.commitProcessedOffset(completedOffset)
 
-	cmdList, completedOffset = p.getNextRawCommandList()
+	cmdList, completedOffset = s.getNextRawCommandList()
 	assert.Equal(t, rawCommandList{
 		ip:   newIPAddr(123, 9, 2, 5),
 		port: 7233,
 		data: []byte("another-data"),
 	}, cmdList)
 
-	p.commitProcessedOffset(completedOffset)
+	s.commitProcessedOffset(completedOffset)
 
-	cmdList, completedOffset = p.getNextRawCommandList()
+	cmdList, completedOffset = s.getNextRawCommandList()
 	assert.Equal(t, rawCommandList{
 		ip:   newIPAddr(89, 0, 3, 6),
 		port: 7000,
 		data: []byte("random-data"),
 	}, cmdList)
 
-	p.commitProcessedOffset(completedOffset)
-	assert.Equal(t, completedOffset, p.getCommitProcessed())
+	s.commitProcessedOffset(completedOffset)
+	assert.Equal(t, completedOffset, s.getCommitProcessed())
 }
 
 func TestCommandListStore_WaitAvailable_Single_Command(t *testing.T) {
-	p := newCommandListStore()
-	p.appendCommands(newIPAddr(192, 168, 0, 1), 8100, []byte("some-data"))
-	continued := p.waitAvailable()
+	s := newCommandListStore()
+	s.appendCommands(newIPAddr(192, 168, 0, 1), 8100, []byte("some-data"))
+	continued := s.waitAvailable()
 	assert.Equal(t, true, continued)
 }
 
 func TestCommandListStore_WaitAvailable_Stopped(t *testing.T) {
-	p := newCommandListStore()
-	p.stopWait()
-	continued := p.waitAvailable()
+	s := newCommandListStore()
+	s.stopWait()
+	continued := s.waitAvailable()
 	assert.Equal(t, false, continued)
 }
 
 func TestCommandListStore_WaitAvailable_No_Command(t *testing.T) {
-	p := newCommandListStore()
+	s := newCommandListStore()
 
 	called := false
 	go func() {
-		p.waitAvailable()
+		s.waitAvailable()
 		called = true
 	}()
 	time.Sleep(1 * time.Millisecond)
@@ -106,7 +105,7 @@ func TestCommandListStore_WaitAvailable_No_Command(t *testing.T) {
 }
 
 func TestCommandListStore_Stress_Test(t *testing.T) {
-	p := newCommandListStoreBuffSize(197)
+	s := newCommandListStoreBuffSize(197)
 
 	const numCommands = 1000
 	count := uint32(0)
@@ -115,13 +114,13 @@ func TestCommandListStore_Stress_Test(t *testing.T) {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		for p.waitAvailable() {
-			cmdList, offset := p.getNextRawCommandList()
+		for s.waitAvailable() {
+			cmdList, offset := s.getNextRawCommandList()
 
 			assert.Equal(t, newIPAddr(198, 168, 53, 1), cmdList.ip)
 			assert.Equal(t, uint16(8765), cmdList.port)
 			assert.Equal(t, []byte("command-no-"), cmdList.data[:len("command-no-")])
-			p.commitProcessedOffset(offset)
+			s.commitProcessedOffset(offset)
 
 			atomic.AddUint32(&count, 1)
 			time.Sleep(1 * time.Microsecond)
@@ -131,16 +130,16 @@ func TestCommandListStore_Stress_Test(t *testing.T) {
 	for i := 0; i < numCommands; i++ {
 		data := []byte(fmt.Sprintf("command-no-%d", i))
 		size := len(data)
-		for !p.isCommandAppendable(size) {
+		for !s.isCommandAppendable(size) {
 			//revive:disable-next-line:empty-block
 		}
-		p.appendCommands(newIPAddr(198, 168, 53, 1), 8765, data)
+		s.appendCommands(newIPAddr(198, 168, 53, 1), 8765, data)
 	}
 
 	for atomic.LoadUint32(&count) < numCommands {
 		//revive:disable-next-line:empty-block
 	}
-	p.stopWait()
+	s.stopWait()
 	wg.Wait()
 
 	assert.Equal(t, uint32(numCommands), atomic.LoadUint32(&count))
